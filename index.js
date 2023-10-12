4dc60736928306773eefeac7e77df6c5472aefbe
iamapusaha <apusaha392@gmail.com>
2023-10-12 12:22:19 +0600

    connected to MongoDB

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors())
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ApuSaha:<password>@cluster0.gnqpnrf.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ApuSaha:JsiUM6jGPcdQkNmp@cluster0.gnqpnrf.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
