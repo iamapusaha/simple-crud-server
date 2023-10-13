f63a2f326ed76fbbed425959b940860a869f39e0
iamapusaha <apusaha392@gmail.com>
2023-10-13 23:02:48 +0600

    post and get data mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://ApuSaha:JsiUM6jGPcdQkNmp@cluster0.gnqpnrf.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,9 +23,17 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        app.post('/users', (req, res) => {
+        const userCollection = client.db("userDB").collection("users");
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result)
         })
 
 
@@ -33,7 +42,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
