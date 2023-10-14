f40f6217c9f491e1e1c4c397381502287cb0ea46
iamapusaha <apusaha392@gmail.com>
2023-10-14 13:54:40 +0600

    get single data by id

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.findOne(query);
+            res.send(result)
+        })
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
